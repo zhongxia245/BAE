b669ac595c60bf4d24a9841896330c80a0b6ed7a
zhongxia245 <zhongxia245@sina.com>
2015-12-18 01:01:42 +0800

    nav可以访问，前端资源页面导航 【使用 handlebar模板+bootstrap】

---
+++ public/nav/js/index.js
@@ -0,0 +1,31 @@
+'use strict';
+(function($, exports) {
+    exports.render = function(selector, tmp_selector, data) {
+        //获取模板
+        var tmp = $(tmp_selector).html();
+        if (tmp) {
+            //注册一个Handlebars模版，通过id找到某一个模版，获取模版的html框架
+            var fn_template = Handlebars.compile(tmp);
+            //将json对象用刚刚注册的Handlebars模版封装，得到最终的html，插入到基础table中。
+            $(selector).html(fn_template(data));
+        } else {
+            console.log('Not Find ID:' + tmp_selector + '> template....');
+        }
+    }
+})($, window);
+// 顶部列表
+$.getJSON('data/nav_top.json', function(json) {
+    window.render('#id_nav_top', '#nav', json);
+});
+// 左侧列表
+$.getJSON('data/nav_left.json', function(json) {
+    window.render('#id_nav_sidebar', '#nav', json);
+});
+// 常用框架集
+$.getJSON('data/kjj.json', function(json) {
+    window.render('#id_kjj', '#nav_kjj', json);
+});
+// 博客列表
+$.getJSON('data/bkll.json', function(json) {
+    window.render('#id_bkll', '#nav_bkll', json);
+});
