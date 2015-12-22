06710152791169054ec66c7aa849ec49cc49a263
zhongxia245 <zhongxia245@sina.com>
2015-12-23 01:54:07 +0800

    修改数据库

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongoosekeeper.config(config.localDb);
+mongoosekeeper.config(config.baeDb);
 
 app.use('/', routes);
 app.use('/', adminRoutes);
