9906f49281e2eedc30d98718f7f9a40352bd8513
huangxiaodong <huangxiaodong@licaigc.com>
2017-02-15 15:03:35 +0800

    update info

--- config.example.js
+++ config.example.js
@@ -3,8 +3,8 @@ module.exports = {
   baeDb: { //线上数据库账号密码
     "host": "mongo.duapp.com",
     "database": "fZelkgarNLTjLCCFIbdX",
-    "userid": "4a2a3087b2bc47a4b7f09e43c41073f3",
-    "password": "9ea7aee739134ba5a9a1819c0f2d1c86",
+    "userid": "",
+    "password": "",
     "port": 8908
   },
   localDb: { //本地数据库账号密码
@@ -15,8 +15,8 @@ module.exports = {
     "port": 27017
   },
   log: {
-    'user': '4a2a3087b2bc47a4b7f09e43c41073f3',
-    'passwd': '836dd0e7456e474f8054e6e11c435786'
+    'user': '',
+    'passwd': ''
   },
   cross_domain: true //是否跨域
 };
